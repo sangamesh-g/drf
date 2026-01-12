2813a17afe9737a5a4200dd3c08b42b7df399b1b
sangamesh <garuna217@gmail.com>
2026-01-12 18:34:45 +0530

    small msg in settings about redis and ccelery

--- ST/ST/settings.py
+++ ST/ST/settings.py
@@ -184,6 +184,9 @@ SESSION_CACHE_ALIAS = "default"
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
+# Redis = Message Broker (queue)
+# Redis = Result Backend (results)
+
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
